66a1a799827b21b0132c83e8c133def3b20e45e0
Thomas Coopman <thomas.coopman@gmail.com>
2022-06-17 15:56:29 +0200

    example 7 with implementation

--- tests.ts
+++ tests.ts
@@ -54,25 +54,55 @@ class PriceCalculationService {
   constructor(visitorRepository: VisitorsRepository) {
     this.visitorRepository = visitorRepository;
   }
-  calculate(id: string) {
-    const visitor = this.visitorRepository.findById(id);
-    const deliveries = visitor!.deliveries;
+  calculateFraction(
+    visitor: Visitor,
+    type: string,
+    deliveries: delivery[],
+    price: number,
+  ) {
     let totalWeight = deliveries.reduce<number>(
       (total, { weight }) => total + weight,
       0,
     );
     let lastWeight = deliveries[deliveries.length - 1]?.weight || 0;
     let previousWeight = totalWeight - lastWeight;
-    if (visitor!.city === "Pineville") {
+    if (visitor.city === "Pineville" && type === "CONSTRUCTION") {
       if (lastWeight == totalWeight) {
         lastWeight = lastWeight - 100;
       } else if (previousWeight > 100) {
         // do nothing
       } else {
-        lastWeight = lastWeight - Math.max((100 - previousWeight), 0)
+        lastWeight = lastWeight - Math.max(100 - previousWeight, 0);
       }
     }
-    return Math.max(lastWeight * 0.1, 0)
+    return Math.max(lastWeight * price, 0);
+  }
+  calculate(id: string) {
+    const visitor = this.visitorRepository.findById(id)!;
+    const deliveries = visitor!.deliveries;
+    const deliveryPerType = deliveries.reduce(
+      (perType: { [key: string]: delivery[] }, delivery) => {
+        if (!perType[delivery.type]) {
+          perType[delivery.type] = [];
+        }
+        perType[delivery.type].push(delivery);
+        return perType;
+      },
+      {},
+    );
+    const pricePerType: { [key: string]: number } = {
+      "CONSTRUCTION": 0.1,
+      "GREEN WASTE": 0.2,
+    };
+    return Object.keys(deliveryPerType).map((type) => {
+      const price: number = pricePerType[type]!;
+      return this.calculateFraction(
+        visitor,
+        type,
+        deliveryPerType[type],
+        price,
+      );
+    }).reduce((sum, price) => sum + price, 0);
   }
 }
 
@@ -238,4 +268,29 @@ Deno.test("calculate price example 6", () => {
 
   // THEN
   assertEquals(price, 0);
+});
+
+Deno.test("calculate price example 7", () => {
+  // PREP
+  const visitorId = "Aiden";
+  const { visitService, priceCalculationService } = testSetup(
+    visitorId,
+    "Pineville",
+  );
+
+  // GIVEN
+  visitService.registerDelivery(visitorId, [{
+    type: "CONSTRUCTION",
+    weight: 25,
+  }]);
+  visitService.registerDelivery(visitorId, [{
+    type: "GREEN WASTE",
+    weight: 100,
+  }]);
+
+  // WHEN
+  const price = priceCalculationService.calculate(visitorId);
+
+  // THEN
+  assertEquals(price, 20);
 });
